2d91489b52a9726eae713e32e5ea6fd1689f80b9
thetrueryan <georgytoper@gmail.com>
2026-06-25 02:34:18 +0400

    add create payment schema

---
+++ src/schemas/payment.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel, Field, field_validator
+
+from src.models.enums import CurrencyType
+
+FORBIDDEN_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "0.0.0.0",
+    "[::1]",       
+    "192.168.",    
+    "172.16.",     
+    "172.17.",
+    "172.18.",
+    "172.19.",
+    "172.20.",
+    "172.30.",
+    "172.31.", 
+    "postgres",
+    "rabbitmq",
+]
+
+
+class CreatePaymentSchema(BaseModel):
+    amount: float = Field(gt=0)
+    currency: CurrencyType = Field(..., description="Currencies: usd, rub, eur")
+    description: str | None = Field(None, max_length=255)
+    meta_info: dict | None = None
+    webhook_url: str = Field(max_length=2048)
+
+    @field_validator("webhook_url")
+    def is_valid_url(cls, value: str) -> str:
+        if value.lower() in FORBIDDEN_HOSTS:
+            raise ValueError("Invalid webhook_url")
+        return value
+
